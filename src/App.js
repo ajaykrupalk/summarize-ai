66f51396429196924179e1ef4cf4646aab817175
ajaykrupalk <ajaykrupalk@outlook.in>
2023-09-26 23:32:12 +0530

    AI Features Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import InputPanel from './components/InputPanel';
 import OutputPanel from './components/OutputPanel';
 import PageHeader from './components/PageHeader';
 
 function App() {
+  const [bookName, setBookName] = useState('');
+
+  const handleInputValue = (bookValue) => {
+    setBookName(bookValue)
+  }
+
   return (
     <div className='w-screen font-sans'>
       <PageHeader/>
       <div className='flex flex-wrap p-5 mt-1 gap-y-10 lg:gap-x-20 xl:gap-x-40'>
-        <div className='w-screen md:w-96 lg:ml-5 xl:ml-40'>
-          <InputPanel />
+        <div className='w-screen md:w-96 lg:ml-5 xl:ml-40 lg:flex lg:items-center'>
+          <InputPanel onInputValue={handleInputValue}/>
         </div>
         <div className='w-screen lg:w-[30em] xl:w-[40em]'>
-          <OutputPanel />
+          <OutputPanel bookName={bookName}/>
         </div>
       </div>     
     </div>
--- src/components/OutputPanel.js
+++ src/components/OutputPanel.js
@@ -1,7 +1,43 @@
-function OutputPanel() {
+import { HuggingFaceStream, StreamingTextResponse } from "ai";
+import { useEffect, useState } from "react";
+
+
+function OutputPanel({ bookName }) {
+    const [bookSummary, setBookSummary] = useState('')
+    useEffect(() => {
+        setBookSummary('')
+        const fetchData = async () => {
+            const response = await fetch(
+                "https://api-inference.huggingface.co/models/gpt2",
+                {
+                    headers: { Authorization: `Bearer ${process.env.REACT_APP_HF_API_KEY}` },
+                    method: "POST",
+                    body: JSON.stringify({
+                        "inputs": `Summarize the book '${bookName}'`,
+                        "parameters": {
+                            "return_full_text": false,
+                            "repetition_penalty": 100.0,
+                            "max_new_tokens": 250,
+                        }
+                    }),
+                }
+            );
+            const result = await response.json();
+            if(result[0].generated_text.length > 0) {
+                console.log('inside')
+                setBookSummary(result[0].generated_text);
+                return;
+            }
+            setBookSummary(' ')
+            return;
+        };
+
+        fetchData();
+    }, [bookName])
+
     return (
-        <div className="h-[35em] w-full bg-slate-100 rounded-md overflow-auto border-2 border-slate-200">
-            
+        <div className="p-3 h-[35em] w-full bg-slate-100 rounded-md overflow-auto border-2 border-slate-200">
+            {bookSummary}
         </div>
     );
 }
